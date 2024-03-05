b5c2a4ee50ce05f4b9968d0118d2eaf05bbaa0d8
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-03-05 22:07:07 +0530

    completed rename functionality of board-card

--- components/actions.tsx
+++ components/actions.tsx
@@ -1,41 +1,80 @@
-"use client"
+"use client";
 
-import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu"
-import { DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuTrigger, DropdownMenuSeparator } from "@/components/ui/dropdown-menu"
-import { useApiMutation } from "@/hooks/use-api-mutation"
-import { Link2, Trash2 } from "lucide-react"
-import { toast } from "sonner"
-import { api } from "@/convex/_generated/api"
-import { ConfirmModal } from "./confitm-modal"
-import { Button } from "./ui/button"
+import { toast } from "sonner";
+import { Link2, Pencil, Trash2 } from "lucide-react";
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
 
-interface ActionsProps{
-    children:React.ReactNode
-    side?:DropdownMenuContentProps["side"]
-    sideOffset?:DropdownMenuContentProps["sideOffset"]
-    id:string
-    title:string
-}
+import { ConfirmModal } from "./confitm-modal"; 
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { Button } from "@/components/ui/button";
+import { useRenameModal } from "@/store/use-rename-modal";
 
-export const Actions = ({children, side,sideOffset, id, title}:ActionsProps) => {
-    const { mutate, pending } = useApiMutation(api.board.remove)
-    const onCopyLink = () => {
-        navigator.clipboard.writeText(`${window.location.origin}/board/${id}`).then(() => toast.success("Link copied"))
-        .catch(() => toast.error("Failed to copy link"))
-    }
-    const onDelete = () => {
-        mutate({id})
-        .then(() => toast.success("Board deleted"))
-        .catch(() => toast.error("Failed to delete board"))
-    }
-    return(
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                {children}
-            </DropdownMenuTrigger>
-            <DropdownMenuContent side={side} sideOffset={sideOffset} className="w-60" onClick={(e) => e.stopPropagation()}>
-                <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}><Link2 className="h-4 w-4 mr-2 "/>Copy board link</DropdownMenuItem>
-                <ConfirmModal
+interface ActionsProps {
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+};
+
+export const Actions = ({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionsProps) => {
+  const { onOpen } = useRenameModal();
+  const { mutate, pending } = useApiMutation(api.board.remove);
+
+  const onCopyLink = () => {
+    navigator.clipboard.writeText(
+      `${window.location.origin}/board/${id}`,
+    )
+      .then(() => toast.success("Link copied"))
+      .catch(() => toast.error("Failed to copy link"))
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board deleted"))
+      .catch(() => toast.error("Failed to delete board"));
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        {children}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        <DropdownMenuItem
+          onClick={onCopyLink}
+          className="p-3 cursor-pointer"
+        >
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => onOpen(id, title)}
+          className="p-3 cursor-pointer"
+        >
+          <Pencil className="h-4 w-4 mr-2" />
+          Rename
+        </DropdownMenuItem>
+        <ConfirmModal
           header="Delete board?"
           description="This will delete the board and all of its contents."
           disabled={pending}
@@ -49,7 +88,7 @@ export const Actions = ({children, side,sideOffset, id, title}:ActionsProps) => 
             Delete
           </Button>
         </ConfirmModal>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
-}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
