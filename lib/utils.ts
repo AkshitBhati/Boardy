fbd2ccd01a9624ff22812509797ca0ef4c657774
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-03-07 18:06:04 +0530

    select layer completing

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { Camera } from "@/types/canvas";
+import { Camera, Color } from "@/types/canvas";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
@@ -28,3 +28,6 @@ export function pointerEventToCanvasPoint(
     }
 }
 
+export function colorToCss(color: Color) {
+  return `#${color.r.toString(16).padStart(2, "0")}${color.g.toString(16).padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`;
+}
